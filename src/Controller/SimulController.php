b0dfa1009a2055e0924e42a2329736bc6de4ebe3
clebeuxestran <clebeux@estran-brest.education>
2019-03-14 11:25:39 +0100

    Affichage des reductions

--- src/Controller/SimulController.php
+++ src/Controller/SimulController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Sejour;
 
 class SimulController extends AbstractController
 {
@@ -12,8 +13,13 @@ class SimulController extends AbstractController
      */
     public function index()
     {
+        $repo = $this->getDoctrine()->getRepository(Sejour::class);
+        $sejours = $repo->findAll();
+        $montant = "";
         return $this->render('simul/simul.html.twig', [
             'controller_name' => 'SimulController',
+            'sejours' => $sejours,
+            'montant' => $montant
         ]);
     }
 }
